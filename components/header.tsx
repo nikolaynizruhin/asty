bad795612fec2ae0a4ddb9045ed8ecc296d64df5
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-09-15 00:49:29 +0300

    Add mobile menu toggle

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import Image from 'next/image'
+import { Dialog } from '@headlessui/react'
+import { useState } from 'react'
 
 export default function Header({ isDark = false }: { isDark?: boolean }) {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
   return (
     <header className="absolute inset-x-0 top-0 z-50">
       <nav
@@ -17,6 +23,7 @@ export default function Header({ isDark = false }: { isDark?: boolean }) {
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+            onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -57,10 +64,10 @@ export default function Header({ isDark = false }: { isDark?: boolean }) {
         </div>
       </nav>
       {/* Mobile menu, show/hide based on menu open state. */}
-      <div className="lg:hidden" role="dialog" aria-modal="true">
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         {/* Background backdrop, show/hide based on slide-over state. */}
         <div className="fixed inset-0 z-50" />
-        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">ASTY</span>
@@ -69,6 +76,7 @@ export default function Header({ isDark = false }: { isDark?: boolean }) {
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-400"
+              onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
               <svg
@@ -125,8 +133,8 @@ export default function Header({ isDark = false }: { isDark?: boolean }) {
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   )
 }
