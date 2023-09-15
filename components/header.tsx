67a8bd7a42ade06f63b466c10567d6ff34d994f0
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-09-15 09:05:58 +0300

    Cleanup

--- components/header.tsx
+++ components/header.tsx
@@ -63,9 +63,7 @@ export default function Header({ isDark = false }: { isDark?: boolean }) {
           </a>
         </div>
       </nav>
-      {/* Mobile menu, show/hide based on menu open state. */}
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        {/* Background backdrop, show/hide based on slide-over state. */}
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
