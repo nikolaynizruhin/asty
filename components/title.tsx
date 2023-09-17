edadf57be3d5bd3ca93faadeadebbfe1eec25602
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-09-17 10:07:55 +0200

    Add key

--- components/title.tsx
+++ components/title.tsx
@@ -1,7 +1,7 @@
 export default function Title({ text, className = '' }: { text: string, className: string }) {
   return (
     <h1 className={`flex justify-between text-[180px] font-bold leading-none text-black opacity-[0.02] ${className}`}>
-      {text.split('').map(character => <span>{character}</span>)}
+      {text.split('').map((character, index) => <span key={index}>{character}</span>)}
     </h1>
   )
 }
