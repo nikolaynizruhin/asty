266b16320dec4dca713de6626decf38d37eb611b
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-09-22 00:21:48 +0200

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,34 +2,32 @@ import Icon from "./icon";
 
 export default function Footer({ isDark = false }: { isDark?: boolean }) {
   return (
-    <footer className={`p-10 lg:px-16 ${isDark ? 'bg-[#1a1a1a]' : 'bg-white'}`}>
-      <a href="#" className="text-[10px] leading-5 text-[#888] mr-[20px]">
+    <footer className={`px-4 pb-11 md:px-8 font-light ${isDark ? 'bg-[#1a1a1a]' : 'bg-white'}`}>
+      <a href="#" className="text-[10px] leading-5 text-white opacity-[0.32] mr-[20px]">
         Privacy Notice
       </a>
-      <a href="#" className="text-[10px] leading-5 text-[#888]">
+      <a href="#" className="text-[10px] leading-5 text-white opacity-[0.32]">
         Terms of use
       </a>
-      <p className="text-[10px] my-3 leading-5 text-[#888] max-w-[460px]">
+      <p className="text-[10px] my-3 leading-5 text-white opacity-[0.32] max-w-[460px]">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
         voluptate officiis veritatis vitae dolor ullam dignissimos nihil placeat
         quisquam ducimus odit amet dolores, ut pariatur voluptas dolorum
         voluptatem quas. Dolorum?
       </p>
-      <div className="md:flex md:items-center md:justify-between">
-        <div className="flex justify-center space-x-6 md:order-2">
-          <a href="https://www.instagram.com/asty_ua" className="text-[#888] hover:text-gray-500">
+      <div className="md:flex md:items-center md:justify-between text-[#888]">
+        <p className="text-[8px] leading-5 font-light">
+          © { new Date().getFullYear() } ASTY, INC. ALL RIGHTS RESERVED.
+        </p>
+        <div className="flex space-x-6 mt-3 md:mt-0">
+          <a href="https://www.instagram.com/asty_ua" className="hover:text-gray-500">
             <span className="sr-only">Instagram</span>
             <Icon name="instagram" />
           </a>
-          <a href="https://www.facebook.com/asty.ukraine" className="text-[#888] hover:text-gray-500">
+          <a href="https://www.facebook.com/asty.ukraine" className="hover:text-gray-500">
             <span className="sr-only">Facebook</span>
             <Icon name="facebook" />
           </a>
-        </div>
-        <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-[10px] leading-5 text-[#888]">
-            © { new Date().getFullYear() } ASTY, INC. ALL RIGHTS RESERVED.
-          </p>
         </div>
       </div>
     </footer>
