08d400ddf79bc2a307cbce4b2916a9ca80568d99
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-10-26 00:17:58 +0200

    Update mobile nav

--- components/header.tsx
+++ components/header.tsx
@@ -59,7 +59,7 @@ export default function Header({ isDark = false }: { isDark?: boolean }) {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[rgba(31,31,31,0.99)] px-6 py-6 sm:max-w-sm">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">ASTY</span>
@@ -91,7 +91,7 @@ export default function Header({ isDark = false }: { isDark?: boolean }) {
             <div className="-my-6 divide-y divide-gray-500/25">
               <div className="space-y-2 py-6">
                 {navigation.map((item, index) => (
-                  <a key={index} href={item.href} className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-800 ${isDark ? 'text-[#333]' : 'text-white'}`}>
+                  <a key={index} href={item.href} className={`-mx-3 block rounded-lg px-3 text-center py-2 text-base font-semibold leading-7 hover:bg-gray-800 ${isDark ? 'text-[#333]' : 'text-white'}`}>
                     {item.name}
                   </a>
                 ))}
