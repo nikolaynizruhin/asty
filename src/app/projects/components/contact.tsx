c98e681fc9e7dc9f07f3b594d635396e65c8ac24
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-11-18 10:39:23 +0100

    Update contact

--- src/app/projects/components/contact.tsx
+++ src/app/projects/components/contact.tsx
@@ -5,10 +5,10 @@ export default function Contact() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-xl xl:max-w-4xl px-6 lg:px-8">
         <div className="mx-auto px-4 max-w-[550px] md:max-w-[464px] lg:mx-0 lg:max-w-none">
-          <h2 className="mt-2 text-3xl font-semibold text-gray-900 sm:text-4xl max-w-[300px] tracking-wide">
+          <h2 className="mt-2 text-[28px] font-semibold text-gray-900 md:text-4xl max-w-[300px] tracking-wide">
             Lets talk about your project!
           </h2>
-          <p className="text-[#666] font-light max-w-[350px] mt-7 mb-24">
+          <p className="text-[#666] font-light max-w-[350px] mt-7 mb-24 text-sm xl:text-base">
             We are ready to lead you into the exciting world of architecture and
             interior design.
           </p>
