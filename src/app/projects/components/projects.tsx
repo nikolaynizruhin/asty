eaeb1d6bb3af050eaf3cd7fe5447c5629c1660b9
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-11-18 23:28:20 +0100

    Update projects

--- src/app/projects/components/projects.tsx
+++ src/app/projects/components/projects.tsx
@@ -38,15 +38,15 @@ const projects = [
 
 export default function Projects() {
   return (
-    <div className="bg-white py-24 xl:pt-56 px-8 xl:px-16">
+    <div className="bg-white py-24 xl:pt-56 px-4 md:px-8 xl:px-16 max-w-[550px] mx-auto md:max-w-none">
       <Title text="ПРОЄКТИ" className="px-[3vw] hidden xl:flex" />
       <div className="text-xs text-[#828282] mt-11">
-        <button className="text-[#121212] mr-24 pr-4 xl:uppercase">All</button>
-        <button className="mr-24 pr-4 xl:uppercase">Architecture</button>
-        <button className="mr-24 pr-4 xl:uppercase">Residental</button>
+        <button className="text-[#121212] mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase">All</button>
+        <button className="mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase">Architecture</button>
+        <button className="mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase">Residental</button>
         <button className="xl:uppercase">Commercial</button>
       </div>
-      <div className="grid grid-cols-4 gap-[2px] mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-[2px] mt-8">
         {projects.map((project, index) => (
           <a key={index} href={project.href}>
             <Image
