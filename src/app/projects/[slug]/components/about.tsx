a4c900bd59e2756d39e544d7d870ce056e17a7db
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-11-19 22:27:43 +0100

    Update project info

--- src/app/projects/[slug]/components/about.tsx
+++ src/app/projects/[slug]/components/about.tsx
@@ -2,18 +2,41 @@ import Icon from "@/src/components/icon"
 import Title from "@/src/components/title"
 import Detail from "./detail"
 
+const details = [
+  {
+    icon: "geo-alt",
+    title: "Location",
+    description: "Madrid, Spain"
+  },
+  {
+    icon: "houses",
+    title: "Square meters",
+    description: "13,993 ft² / 1,300m²"
+  },
+  {
+    icon: "calendar3",
+    title: "Date",
+    description: "In progress"
+  },
+  {
+    icon: "people",
+    title: "Team",
+    description: "Artur Sharf"
+  }
+]
+
 export default function About() {
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <Title text="ABOUT" className="-mb-28 mx-[9vw]" />
+    <div className="bg-white py-24 sm:py-40">
+      <Title text="ABOUT" className="-mb-40 mx-[9vw]" />
       <div className="mx-auto max-w-4xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
           <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 tracking-wide lg:max-w-none lg:grid-cols-2">
             <div>
-              <h2 className="mb-12 text-3xl font-semibold text-gray-900 sm:text-3xl max-w-[300px]">
+              <h2 className="mb-6 text-3xl font-semibold text-gray-900 sm:text-4xl max-w-[300px]">
                 Brief
               </h2>
-              <p className="text-sm font-light leading-6 text-[#666]">
+              <p className="text-base font-light leading-6 text-[#666]">
                 ASTY created this design project for a young family. The customer
                 and his wife love and adore Spain, and they also consider their
                 home a place with a warm atmosphere for leisure time. That is why
@@ -23,11 +46,8 @@ export default function About() {
               </p>
             </div>
             <div className="flex flex-col gap-7 pt-10 pl-24">
-              <Detail icon="geo-alt" title="Location" description="Madrid, Spain" />
-              <Detail icon="houses" title="Square meters" description="13,993 ft² / 1,300m²" />
-              <Detail icon="calendar3" title="Date" description="In progress" />
-              <Detail icon="people" title="Team" description="Artur Sharf" />
-              <div className="text-sm font-light leading-6 text-[#666] flex items-center mt-16">
+              {details.map((detail, index) => <Detail key={index} {...detail} />)}
+              <div className="text-base font-light leading-6 text-[#666] flex items-center mt-16">
                 Share:
                 <Icon name="facebook" width={12} height={12} className="ml-7 mr-6" />
                 <Icon name="instagram" width={12} height={12} />
--- src/app/projects/[slug]/components/detail.tsx
+++ src/app/projects/[slug]/components/detail.tsx
@@ -5,7 +5,7 @@ export default function Detail({ title, description, icon }: { title: string, de
     <div className="flex">
       <Icon name={icon} className="mx-9" />
       <div>
-        <h4 className="text-[#333] font-medium tracking-wider mb-1 leading-4 text-sm">
+        <h4 className="text-[#333] font-semibold tracking-wider mb-1 leading-4 text-sm">
           {title}:
         </h4>
         <p className="text-[#666] leading-4 tracking-wider text-sm font-light">
