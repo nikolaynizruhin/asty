addfb1cfb94f634b0355f4adac215327ffdf7847
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-11-26 11:11:44 +0100

    Update gallery

--- src/app/projects/[slug]/components/images.tsx
+++ src/app/projects/[slug]/components/images.tsx
@@ -2,63 +2,59 @@ import Image from 'next/image'
 
 export default function Images() {
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-full px-6 lg:px-16">
-        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-sm tracking-wide font-light leading-6 text-[#666] lg:max-w-none lg:grid-cols-2">
-            <Image
-              src="https://yodezeen.com/wp-content/uploads/2023/04/Untitled-2-05.jpg"
-              width="640"
-              height="905"
-              alt=""
-            />
-            <Image
-              src="https://yodezeen.com/wp-content/uploads/2023/04/Untitled-211111111-02.jpg"
-              width="640"
-              height="905"
-              alt=""
-            />
-            <Image
-              src="https://yodezeen.com/wp-content/uploads/2023/04/55555.jpg"
-              width="640"
-              height="905"
-              alt=""
-            />
-            <div className="flex flex-col justify-center px-28">
-              <h2 className="text-3xl font-semibold text-gray-900 sm:text-3xl tracking-wide mb-7">
-                Concept
-              </h2>
-              <p>
-                As often happens in the real estate market of Europe, YODEZEEN
-                received an already-built house with existing walls, window
-                openings, and facade architecture. Therefore, the task was to
-                modernize the planning as much as possible and ergonomically place
-                all the owners’ needs.
-              </p>
-              <p>
-                The house has a center — an elevator surrounded by a phyto-box. On
-                the first floor, there is a living room with a bar and a wine
-                zone, a gym, a spa, a cinema, and an exit to the garage. The main
-                floor has a kitchen, dining area, two-format living room, office,
-                and two guest bedrooms. On the upper floor, the architects have
-                placed a master unit with three dressing areas and a big bathroom,
-                a kids’ unit, and a bedroom exclusively for the owner’s parents.
-              </p>
-            </div>
-            <Image
-              src="https://yodezeen.com/wp-content/uploads/2023/04/%D0%BD%D0%BE%D1%80%D0%BC-02.jpg"
-              width="640"
-              height="905"
-              alt=""
-            />
-            <Image
-              src="https://yodezeen.com/wp-content/uploads/2023/04/%D0%BD%D0%BE%D1%80%D0%BC-01.jpg"
-              width="640"
-              height="905"
-              alt=""
-            />
-          </div>
+    <div className="bg-white py-12 xl:py-20">
+      <div className="mx-auto justify-items-center max-w-[550px] px-4 xl:px-16 grid grid-cols-1 gap-x-5 xl:gap-x-10 gap-y-4 md:gap-y-10 xl:gap-y-16 text-[#666] md:max-w-full md:grid-cols-2">
+        <Image
+          src="https://yodezeen.com/wp-content/uploads/2023/04/Untitled-2-05.jpg"
+          width="640"
+          height="905"
+          alt=""
+        />
+        <Image
+          src="https://yodezeen.com/wp-content/uploads/2023/04/Untitled-211111111-02.jpg"
+          width="640"
+          height="905"
+          alt=""
+        />
+        <Image
+          src="https://yodezeen.com/wp-content/uploads/2023/04/55555.jpg"
+          width="640"
+          height="905"
+          alt=""
+        />
+        <div className="flex flex-col justify-center px-28">
+          <h2 className="text-3xl font-semibold text-gray-900 md:text-4xl mb-7">
+            Concept
+          </h2>
+          <p className="font-light">
+            As often happens in the real estate market of Europe, YODEZEEN
+            received an already-built house with existing walls, window
+            openings, and facade architecture. Therefore, the task was to
+            modernize the planning as much as possible and ergonomically place
+            all the owners’ needs.
+          </p>
+          <p className="font-light">
+            The house has a center — an elevator surrounded by a phyto-box. On
+            the first floor, there is a living room with a bar and a wine
+            zone, a gym, a spa, a cinema, and an exit to the garage. The main
+            floor has a kitchen, dining area, two-format living room, office,
+            and two guest bedrooms. On the upper floor, the architects have
+            placed a master unit with three dressing areas and a big bathroom,
+            a kids’ unit, and a bedroom exclusively for the owner’s parents.
+          </p>
         </div>
+        <Image
+          src="https://yodezeen.com/wp-content/uploads/2023/04/%D0%BD%D0%BE%D1%80%D0%BC-02.jpg"
+          width="640"
+          height="905"
+          alt=""
+        />
+        <Image
+          src="https://yodezeen.com/wp-content/uploads/2023/04/%D0%BD%D0%BE%D1%80%D0%BC-01.jpg"
+          width="640"
+          height="905"
+          alt=""
+        />
       </div>
     </div>
   )
--- src/app/projects/[slug]/components/planning.tsx
+++ src/app/projects/[slug]/components/planning.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Planning() {
   return (
     <div className="bg-white">
-      <div className="mx-auto px-6 lg:px-16 mt-10 grid max-w-xl grid-cols-1 justify-items-center gap-8 md:max-w-none xl:grid-cols-2">
+      <div className="mx-auto px-4 lg:px-16 mt-10 grid max-w-xl grid-cols-1 justify-items-center gap-8 md:max-w-none xl:grid-cols-2">
         <Image
           src="https://yodezeen.com/wp-content/uploads/2023/04/Villa-Madri.png"
           width="640"
