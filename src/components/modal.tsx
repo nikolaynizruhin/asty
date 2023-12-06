8d5fac9ee7a8873f9aa81bb637017489ac4c4fe7
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-06 13:25:57 +0100

    Update modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -21,7 +21,7 @@ export default function Modal({ open, setOpen, title, children }: { open: boolea
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full justify-center text-center items-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -31,15 +31,15 @@ export default function Modal({ open, setOpen, title, children }: { open: boolea
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden bg-[#1f1f1f] text-left transition-all sm:my-8 w-full sm:max-w-md md:max-w-3xl xl:max-w-5xl px-4 py-14 md:px-24 md:py-12 xl:px-28 xl:py-20">
-                <div className="absolute right-0 top-0 pr-7 pt-8">
+              <Dialog.Panel className="relative transform overflow-hidden bg-[#1f1f1f] text-left transition-all sm:my-8 max-w-md md:max-w-3xl xl:max-w-5xl px-4 py-14 md:px-24 md:py-12 xl:px-28 xl:py-20">
+                <div className="absolute right-0 top-0 pr-3 pt-4 md:pr-7 md:pt-8">
                   <button
                     type="button"
                     className="text-white"
                     onClick={() => setOpen(false)}
                   >
                     <span className="sr-only">Close</span>
-                    <Icon name="x-lg" width={24} height={24} />
+                    <Icon className="w-4 h-4 md:h-6 md:w-6" name="x-lg" width={24} height={24} />
                   </button>
                 </div>
                 <div>
