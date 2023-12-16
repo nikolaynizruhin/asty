79580a0c0e632238c0818cb2d401467f6a3ae894
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-16 22:51:42 +0100

    Update thank modal

--- src/components/thank-modal.tsx
+++ src/components/thank-modal.tsx
@@ -6,12 +6,14 @@ import Title from "@/src/components/title";
 export default function ThankModal({ open, setOpen }: { open: boolean; setOpen: (open: boolean) => void }) {
   return (
     <Modal open={open} setOpen={setOpen}>
-      <Title text="THANKS" className="opacity-[0.15]" />
-      <div className="xl:pl-[50%] -mt-24">
-        <h2 className="text-2xl font-bold text-white md:text-4xl max-w-[250px] mb-6">
-          Thank you for your message!
-        </h2>
-        <p className="text-[#666] font-light text-base max-w-[350px]">We will reply to you as soon as possible</p>
+      <div className="my-24">
+        <Title text="THANKS" className="opacity-[0.15]" />
+        <div className="xl:pl-[50%] -mt-24">
+          <h2 className="text-2xl font-bold text-white md:text-4xl max-w-[250px] mb-6">
+            Thank you for your message!
+          </h2>
+          <p className="text-[#666] font-light text-base max-w-[350px]">We will reply to you as soon as possible</p>
+        </div>
       </div>
     </Modal>
   )
