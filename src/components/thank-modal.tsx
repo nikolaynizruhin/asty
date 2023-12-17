57cc81ee2f5782168a7c03ce8c6f5dc0c0712181
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-17 01:34:10 +0100

    Cleanup

--- src/components/thank-modal.tsx
+++ src/components/thank-modal.tsx
@@ -7,9 +7,9 @@ export default function ThankModal({ open, setOpen }: { open: boolean; setOpen: 
   return (
     <Modal open={open} setOpen={setOpen}>
       <div className="my-24">
-        <Title text="THANKS" className="opacity-[0.15]" />
-        <div className="xl:pl-[50%] -mt-24">
-          <h2 className="text-2xl font-bold text-white md:text-4xl max-w-[250px] mb-6">
+        <Title text="THANKS" className="opacity-[0.15] lg:-mr-16 lg:ml-48 xl:-mx-16 lg:text-[8.5vw] xl:text-[12.5vw]" />
+        <div className="lg:pl-[50%] -mt-24">
+          <h2 className="text-2xl font-bold text-white md:text-4xl xl:max-w-[250px] mb-8">
             Thank you for your message!
           </h2>
           <p className="text-[#666] font-light text-base max-w-[350px]">We will reply to you as soon as possible</p>
