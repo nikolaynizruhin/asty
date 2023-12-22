cd9ada5bb53b8a521e455e2fe087a4a5589a094b
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-22 15:56:24 +0100

    Add hover effect

--- src/app/projects/components/projects.tsx
+++ src/app/projects/components/projects.tsx
@@ -15,9 +15,10 @@ export default function Projects() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-[2px] mt-8">
         {projects.map((project, index) => (
-          <Link key={index} href={`/projects/${project.slug}`}>
+          <Link key={index} href={`/projects/${project.slug}`} className='group relative'>
+            <h2 className='text-[rgb(242,242,242)] p-6 text-2xl font-bold absolute bottom-0 z-10 opacity-0 group-hover:opacity-100 transition-all duration-300'>{project.title}</h2>
             <Image
-              className="object-cover aspect-[325/232] w-full brightness-[.8] hover:brightness-[0.3] transition-all duration-300"
+              className="object-cover aspect-[325/232] w-full brightness-[.8] group-hover:brightness-[0.3] transition-all duration-300"
               src={`/images/projects/${project.slug}/hero.jpg`}
               width="325"
               height="232"
