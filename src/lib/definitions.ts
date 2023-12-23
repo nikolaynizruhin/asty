fa894550c940a8d726190339cd326bc469f388c6
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-23 13:59:40 +0100

    Update definitions

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,15 +1,14 @@
 export interface Detail {
-    icon: string;
-    title: string;
-    description: string;
-  }
+  icon: string;
+  title: string;
+  description: string;
+}
   
-  export interface Project {
-    id: number;
-    slug: string;
-    name: string;
-    description: string;
-    concept: string;
-    details: Detail[];
-  }
-  
+export interface Project {
+  id: number;
+  slug: string;
+  name: string;
+  description: string;
+  concept: string;
+  details: Detail[];
+}
