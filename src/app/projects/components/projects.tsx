16ba7ffd235806c3c403f7c4a2551bcc9c223e43
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-23 16:40:36 +0100

    Fix scroll

--- src/app/projects/components/projects.tsx
+++ src/app/projects/components/projects.tsx
@@ -13,10 +13,10 @@ export default function Projects({ category }: { category?: Category }) {
     <div className="bg-white pt-24 xl:pt-56 px-4 md:px-8 xl:px-16 max-w-[550px] mx-auto md:max-w-none">
       <Title text="ПРОЄКТИ" className="px-[3vw] hidden xl:flex" />
       <div className="text-xs text-[#828282] mt-11">
-        <Link href='/projects' className={`${category ? '' : 'text-[#121212]' } mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase font-semibold`}>Всі</Link>
-        <Link href='/projects?category=architecture' className={`${category === 'architecture' ? 'text-[#121212]' : '' } mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase font-semibold`}>Архітектура</Link>
-        <Link href='/projects?category=interior' className={`${category === 'interior' ? 'text-[#121212]' : '' } mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase font-semibold`}>Інтерʼєр</Link>
-        <Link href='/projects?category=commerce' className={`${category === 'commerce' ? 'text-[#121212]' : '' } xl:uppercase font-semibold`}>Комерція</Link>
+        <Link href='/projects' scroll={false} className={`${category ? '' : 'text-[#121212]' } mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase font-semibold`}>Всі</Link>
+        <Link href='/projects?category=architecture' scroll={false} className={`${category === 'architecture' ? 'text-[#121212]' : '' } mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase font-semibold`}>Архітектура</Link>
+        <Link href='/projects?category=interior' scroll={false} className={`${category === 'interior' ? 'text-[#121212]' : '' } mr-5 md:mr-12 xl:mr-24 pr-4 xl:uppercase font-semibold`}>Інтерʼєр</Link>
+        <Link href='/projects?category=commerce' scroll={false} className={`${category === 'commerce' ? 'text-[#121212]' : '' } xl:uppercase font-semibold`}>Комерція</Link>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-[2px] mt-8">
         {filteredProjects.map((project, index) => (
