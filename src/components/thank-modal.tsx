17c87af7845aa8a00363fb4f7b7d9a5e23a35068
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-23 18:51:28 +0100

    Update thank modal

--- src/components/thank-modal.tsx
+++ src/components/thank-modal.tsx
@@ -6,10 +6,10 @@ import Title from "@/src/components/title";
 export default function ThankModal({ open, setOpen }: { open: boolean; setOpen: (open: boolean) => void }) {
   return (
     <Modal open={open} setOpen={setOpen}>
-      <div className="my-24">
-        <Title text="ДЯКУЄМО" className="opacity-[0.15] lg:-mr-16 lg:ml-48 xl:-mx-16 lg:text-[8.5vw] xl:text-[12.5vw]" />
-        <div className="lg:pl-[50%] -mt-24">
-          <h2 className="text-2xl font-bold text-white md:text-4xl xl:max-w-[250px] mb-8">
+      <div className="my-28 md:my-24">
+        <Title text="ДЯКУЄМО" className="opacity-[0.15] md:-mr-16 md:ml-32 xl:-mx-16 md:text-[8.5vw] xl:text-[12.5vw] hidden md:flex" />
+        <div className="md:pl-[50%] md:-mt-24 mt-9">
+          <h2 className="text-3xl font-bold text-white md:text-4xl max-w-[250px] mb-4 md:mb-8">
             Дякуємо за повідомлення!
           </h2>
           <p className="text-[#666] font-light text-base max-w-[350px]">Ми звʼяжемося з Вами найближчим часом</p>
