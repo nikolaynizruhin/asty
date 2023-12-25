bd1389e434b07a2ad40e9c3750770c3154fcc857
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-25 11:32:19 +0100

    Update contact

--- src/app/projects/components/contact.tsx
+++ src/app/projects/components/contact.tsx
@@ -13,15 +13,14 @@ export default function Contact() {
     <div className="bg-white pt-12 pb-14 md:pb-24 md:pt-36 xl:pb-44 xl:pt-52">
       <div className="mx-auto max-w-[550px] md:max-w-[464px] xl:max-w-4xl px-4 lg:px-8">
         <h2 className="mt-2 text-3xl font-bold text-gray-900 md:text-4xl max-w-[300px]">
-          Lets talk about your project!
+          Напишіть нам про Ваш проєкт!
         </h2>
         <p className="text-[#666] font-light max-w-[350px] mt-10 md:mt-14 xl:mt-10 xl:mb-28 text-sm xl:text-base">
-          We are ready to lead you into the exciting world of architecture and
-          interior design.
+          Разом ми створемо простір в якому хочеться жити та творити.
         </p>
         <div className="mt-8 md:mt-14 flex">
           <Button onClick={() => setOpenContact(true)}>
-            SEND REQUEST
+            НАПИСАТИ
           </Button>
           <ContactModal open={openContact} setOpen={setOpenContact} setOpenThank={setOpenThank} />
           <ThankModal open={openThank} setOpen={setOpenThank} />
