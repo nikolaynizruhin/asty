5418a9f79c8b16eb457b69e6227ff7f720e07bdb
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-25 13:24:00 +0100

    Update category link

--- src/app/components/categories.tsx
+++ src/app/components/categories.tsx
@@ -23,18 +23,20 @@ export default function Categories() {
   return (
     <div className="mx-auto grid auto-rows-fr grid-cols-1 lg:max-w-none md:grid-cols-3 max-w-xl md:max-w-none">
       {categories.map((category, index) => (
-        <div key={index} className="relative isolate flex flex-col justify-end overflow-hidden bg-gray-900 px-4 py-20 md:py-44 xl:py-96">
-          <Image
-            src={category.image}
-            width="480"
-            height="823"
-            alt=""
-            className="absolute inset-0 -z-10 h-full w-full object-cover brightness-[.3]"
-          />
-          <h3 className="text-2xl xl:text-4xl md:text-center font-bold text-white">
-            <Link href={category.href}>{category.name}</Link>
-          </h3>
-        </div>
+        <Link href={category.href}>
+          <div key={index} className="relative isolate flex flex-col justify-end overflow-hidden bg-gray-900 px-4 py-20 md:py-44 xl:py-96">
+            <Image
+              src={category.image}
+              width="480"
+              height="823"
+              alt=""
+              className="absolute inset-0 -z-10 h-full w-full object-cover brightness-[.3]"
+            />
+            <h3 className="text-2xl xl:text-4xl md:text-center font-bold text-white">
+              {category.name}
+            </h3>
+          </div>
+        </Link>
       ))}
     </div>
   )
