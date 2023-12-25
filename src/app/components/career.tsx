7bd8ebcbfd66dc5a40baf4810fc1b203e91e1f99
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-25 22:13:10 +0100

    Fix title

--- src/app/components/career.tsx
+++ src/app/components/career.tsx
@@ -14,7 +14,7 @@ export default function Career() {
     <div id="career" className="bg-white py-14 sm:py-48">
       <Title text="КАР’ЄРА" className="-mb-[8vw] 2xl:-mb-[10vw] mx-[9vw] hidden md:flex" />
       <div className="mx-auto max-w-lg xl:max-w-4xl px-4">
-        <h2 className="mt-2 text-2xl font-bold text-gray-900 md:text-4xl max-w-[250px]">
+        <h2 className="mt-2 text-3xl font-bold text-gray-900 md:text-4xl max-w-[250px]">
           Стати частиною нашої команди
         </h2>
         <div className="mt-7 md:mt-10 grid grid-cols-1 gap-3 xl:gap-8 text-sm xl:text-base font-light text-[#666] xl:grid-cols-2">
