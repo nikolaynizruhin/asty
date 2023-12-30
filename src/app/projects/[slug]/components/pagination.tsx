2c79a6aabbffceebd8f8c16399a21eb8f0193cd0
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2023-12-30 23:02:45 +0100

    Fix pagination

--- src/app/projects/[slug]/components/pagination.tsx
+++ src/app/projects/[slug]/components/pagination.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 
 export default function Pagination({ project }: { project: Project }) {
   return (
-    <div className="flex text-sm xl:text-lg text-white md:font-bold px-4 md:px-8 xl:px-16 max-w-[550px] mx-auto md:max-w-none bg-[#1a1a1a] justify-between pt-14 pb-14 xl:pb-28">
-      <Link href={`/projects/${getPreviousProject(project)?.slug}`}>
-        Попередній проєкт
-      </Link>
-      <Link href={`/projects/${getNextProject(project)?.slug}`}>
-        Наступний проєкт
-      </Link>
+    <div className="bg-[#1a1a1a]">
+      <div className="flex text-sm xl:text-lg text-white md:font-bold px-4 md:px-8 xl:px-16 max-w-[550px] mx-auto md:max-w-none justify-between pt-14 pb-14 xl:pb-28">
+        <Link href={`/projects/${getPreviousProject(project)?.slug}`}>
+          Попередній проєкт
+        </Link>
+        <Link href={`/projects/${getNextProject(project)?.slug}`}>
+          Наступний проєкт
+        </Link>
+      </div>
     </div>
   )
 }
