a3d6f45610ffa503e6df5e75efa155dbba05b8a8
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2024-01-28 02:15:42 +0100

    Add privacy policy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Icon from "./icon";
 import SocialIcons from "./social-icons";
 
@@ -5,12 +6,12 @@ export default function Footer({ isDark = false }: { isDark?: boolean }) {
   return (
     <footer className={`font-light ${isDark ? 'bg-[#1a1a1a]' : 'bg-white'}`}>
       <div className="max-w-[550px] mx-auto md:max-w-none px-4 md:px-8 xl:px-16 lg:pb-9 pb-11">
-        <a href="#" className={`text-[10px] tracking-widest ${isDark ? 'text-white opacity-[0.32]' : 'text-[#888]'} mr-[20px]`}>
+        <Link href="/privacy" className={`text-[10px] tracking-widest ${isDark ? 'text-white opacity-[0.32]' : 'text-[#888]'} mr-[20px]`}>
           Правові застереження
-        </a>
-        <a href="#" className={`text-[10px] tracking-widest ${isDark ? 'text-white opacity-[0.32]' : 'text-[#888]'}`}>
+        </Link>
+        <Link href="/privacy" className={`text-[10px] tracking-widest ${isDark ? 'text-white opacity-[0.32]' : 'text-[#888]'}`}>
           Політика конфіденційності
-        </a>
+        </Link>
         <div className="md:flex md:items-center md:justify-between text-[#888]">
           <p className="text-[10px] font-light tracking-widest">
             © { new Date().getFullYear() } ASTY, INC. ALL RIGHTS RESERVED.
