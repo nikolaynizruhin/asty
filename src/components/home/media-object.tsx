05e5c362aa722efb15a0be3c0512f8211c455da7
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2024-02-03 16:44:03 +0100

    Cleanup

--- src/components/home/media-object.tsx
+++ src/components/home/media-object.tsx
@@ -8,7 +8,7 @@ export default function MediaObject({ media, isDark = false }: { media: MediaObj
       <dt className={`flex md:items-center gap-x-3 text-sm font-bold min-h-[48px] ${isDark ? 'text-gray-900' : 'text-white'}`}>
         <Icon name={media.icon} width={32} height={32} className="flex-none" />
         <span className="hidden md:inline">
-          {media.id ? <># {media.id}<br />{media.name}</> : media.name}
+          {media?.id ? <># {media.id}<br />{media.name}</> : media.name}
         </span>
       </dt>
       <dd className={`md:mt-6 text-base font-light ${isDark ? 'text-[#666]' : 'text-white'}`}>
