aaa87009b8d1cbefd7f410ddbc6f31ef44b965da
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2024-02-19 00:07:21 +0100

    Add validation errors

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -3,16 +3,16 @@ import { z } from 'zod'
 const terms = z.any().refine(terms => terms, 'Будь ласка, погодьтеся на обробку персональних даних, щоб відправити повідомлення.')
 
 export const contactSchema = z.object({
-  name: z.string().min(3),
-  email: z.string().email(),
-  message: z.string().min(10),
+  name: z.string().min(1, { message: "Поле ім’я є обов’язковим для заповнення." }),
+  email: z.string().email({ message: 'Введіть коректну пошту.' }),
+  message: z.string().min(1, { message: "Поле повідомлення є обов’язковим для заповнення." }),
   terms
 })
 
 export const careerSchema = z.object({
-  name: z.string().min(3),
-  email: z.string().email(),
-  phone: z.string().min(10),
+  name: z.string().min(1, { message: "Поле ім’я є обов’язковим для заповнення." }),
+  email: z.string().email({ message: 'Введіть коректну пошту.' }),
+  phone: z.string().min(10, { message: 'Введіть коректний номер телефону.' }),
   resume: z.any()
     .refine(file => file?.size <= 5000000, 'PDF файл не повинен бути більше ніж 5 MB.')
     .refine(file => file?.type === 'application/pdf', "Підтримується тільки .pdf формат."),
