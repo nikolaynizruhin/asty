ac92af936018cd6db71975dcdc9566c7571efff4
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2024-03-02 00:33:40 +0100

    Update project header

--- components/project/hero.tsx
+++ components/project/hero.tsx
@@ -17,7 +17,7 @@ export default function Hero({ project }: { project: Project }) {
         />
         <Container className="pb-96 pt-60 sm:pb-44 sm:pt-36 xl:pb-96 xl:pt-60">
           <h1 className="text-4xl md:text-5xl font-bold text-white xl:text-7xl">
-            {project.name}
+            {project.title}
           </h1>
           <p className="mt-4 md:mt-6 font-light text-sm xl:text-base text-white">
             {project.description}
