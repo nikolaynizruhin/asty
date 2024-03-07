4229c4fd19f156c9a11887a9b767ef2683975481
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2024-03-07 14:03:58 +0100

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,17 +6,27 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const projects = projectList.map(project => ({
     url: `${app.url}/project/${project.slug}`,
     lastModified: project.updatedAt,
-  }));
+  }))
+
+  const categories = ['architecture', 'interior', 'commerce'].map(category => ({
+    url: `${app.url}/projects/${category}`,
+    lastModified: '2024-03-05',
+  }))
 
   return [
     {
       url: app.url,
-      lastModified: '2024-01-01',
+      lastModified: '2024-03-05',
     },
     {
       url: `${app.url}/contact`,
-      lastModified: '2024-01-01',
+      lastModified: '2024-03-05',
     },
+    {
+      url: `${app.url}/projects`,
+      lastModified: '2024-03-05',
+    },
+    ...categories,
     ...projects,
   ];
 }
