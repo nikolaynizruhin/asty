9f1e0119f2a1b033b2318240d65044aa27ec6a5f
Mykola Nizriukhin <lgnexus4fun@gmail.com>
2026-08-22 13:05:30 +0200

    Update zod

--- lib/validations.ts
+++ lib/validations.ts
@@ -3,16 +3,28 @@ import { z } from 'zod'
 const terms = z.any().refine(terms => terms, 'Будь ласка, погодьтеся на обробку персональних даних, щоб відправити повідомлення.')
 
 export const contactSchema = z.object({
-  name: z.string().min(1, { message: "Поле ім’я є обов’язковим." }),
-  email: z.string().email({ message: 'Введіть коректну пошту.' }),
-  message: z.string().min(1, { message: "Поле повідомлення є обов’язковим." }),
+  name: z.string().min(1, {
+      error: "Поле ім’я є обов’язковим."
+}),
+  email: z.email({
+        error: 'Введіть коректну пошту.'
+  }),
+  message: z.string().min(1, {
+      error: "Поле повідомлення є обов’язковим."
+}),
   terms
 })
 
 export const careerSchema = z.object({
-  name: z.string().min(1, { message: "Поле ім’я є обов’язковим." }),
-  email: z.string().email({ message: 'Введіть коректну пошту.' }),
-  phone: z.string().min(10, { message: 'Введіть коректний номер телефону.' }).optional().or(z.literal('')),
+  name: z.string().min(1, {
+      error: "Поле ім’я є обов’язковим."
+}),
+  email: z.email({
+        error: 'Введіть коректну пошту.'
+  }),
+  phone: z.string().min(10, {
+      error: 'Введіть коректний номер телефону.'
+}).optional().or(z.literal('')),
   resume: z.any()
     .refine(file => file?.size <= 5000000, 'PDF файл не повинен бути більше ніж 5 MB.')
     .refine(file => file?.type === 'application/pdf', "Підтримується тільки .pdf формат."),
