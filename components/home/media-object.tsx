928bd3aa1e7e9e1e939fae3611869e31ddaafbc9
Mykola Nizriukhin <lgnexus4fun@gmail.com>
2026-08-23 12:14:10 +0200

    Fix html

--- components/home/media-object.tsx
+++ components/home/media-object.tsx
@@ -10,14 +10,17 @@ export default function MediaObject({
 }) {
   return (
     <div className="relative flex gap-7 md:flex-col md:gap-0">
-      {media?.id && (
-        <div className="absolute -top-24 -left-20 -z-10 hidden text-[180px] font-bold text-black/[.02] md:block">
-          {media.id}
-        </div>
-      )}
       <dt
-        className={`flex min-h-[48px] gap-x-3 text-sm font-bold md:items-center ${isDark ? "text-gray-900" : "text-white"}`}
+        className={`flex min-h-12 gap-x-3 text-sm font-bold md:items-center ${isDark ? "text-gray-900" : "text-white"}`}
       >
+        {media?.id && (
+          <div
+            aria-hidden="true"
+            className="absolute -top-24 -left-20 -z-10 hidden text-[180px] font-bold text-black/2 md:block"
+          >
+            {media.id}
+          </div>
+        )}
         <Icon name={media.icon} width={32} height={32} className="flex-none" />
         <span className="hidden md:inline">
           {media?.id ? (
@@ -35,7 +38,7 @@ export default function MediaObject({
         className={`text-base font-light md:mt-6 ${isDark ? "text-[#666]" : "text-white"}`}
       >
         <p
-          className={`mb-[10px] text-sm font-bold md:hidden ${isDark ? "text-gray-900" : "text-white"}`}
+          className={`mb-2.5 text-sm font-bold md:hidden ${isDark ? "text-gray-900" : "text-white"}`}
         >
           {media?.id ? `#${media.id} ${media.name}` : media.name}
         </p>
